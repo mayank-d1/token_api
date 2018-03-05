a958be610e0d74081d0299e9cf3616796ff8ebf6
Mayank Diwedi <diwedi.mayank1@gmail.com>
2018-03-05 19:02:05 +0530

    displaying user info by token and user id

--- user_details/views.py
+++ user_details/views.py
@@ -2,15 +2,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import User, Token
 from rest_framework import status
-import ast,json
 from .serializers import UserSerializer
 
 
 class UserList(APIView):
     def get(self, request):
-        user1 = User.objects.all()
-        serializer = UserSerializer(user1, many=True)
-        return Response(serializer.data)
+        query = self.request.GET.get('token')
+        us_id = self.request.GET.get('user')
+        token = Token.objects.get(token=query)
+        user_id = Token.objects.get(user_id=us_id)
+        if token == user_id:
+            user1 = User.objects.filter(id=token.user_id)
+            serializer = UserSerializer(user1, many=True)
+            return Response(serializer.data)
+        else:
+            return Response(status.HTTP_401_UNAUTHORIZED)
 
     def post(self, request):
         serializers = UserSerializer(data=request.data)
